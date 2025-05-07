d9493fa71bb0929292afb50952a0392f1fc3635e
Abhinay Kushwaha <abhinayarkb@gmail.com>
2025-05-07 16:35:28 +0530

    Done

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {Text, View} from 'react-native';
 
 function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500 flex">
+    <View className="flex-1 items-center justify-center bg-pink-500 flex">
       <Text className="text-white text-xl">Hello world</Text>
     </View>
   );
